c9417be894ae0c5ecb384a16ef4528466f475690
Azamat Jumabekov <azamatjumma@MacBook-Air.local>
2021-10-10 05:25:11 +0600

    third commit

--- applications/advertisements/serializers.py
+++ applications/advertisements/serializers.py
@@ -1,5 +1,9 @@
+from django.core.mail import send_mail
 from rest_framework import serializers
 from .models import Advertisement
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class AdvertisementSerializer(serializers.ModelSerializer):
@@ -7,13 +11,15 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         model = Advertisement
         fields = '__all__'
 
-
-class AdvertisementsHistory(serializers.ModelSerializer):
-    class Meta:
-        model = Advertisement
-        fields = '__all__'
-
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
-    #     representation['history'] = AdvertisementSerializer(instance.seller).data
-    #     return representation
+    def create(self, validated_data):
+        request = self.context.get('request')
+        user = request.user
+        validated_data['user'] = user
+        add = Advertisement.objects.create(user=user)
+        add.save()
+        admins_email = list(User.objects.filter(is_staff=True).values_list('email', flat=True))
+        send_mail('Новый заказ',
+                  f'Описание: {add}\nПользователь: {user}',
+                  "azamatjumma@gmail.com",
+                  [user for user in admins_email].append(user.email))
+        return add
